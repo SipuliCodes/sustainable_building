42c06892ad1718a7d12fea39d51267fa31ddcfae
Robi Johansson <rnjoha@utu.fi>
2024-05-04 14:14:48 +0300

    CSS muutoksia

--- src/components/Leaderboard/LeaderboardPlace/index.jsx
+++ src/components/Leaderboard/LeaderboardPlace/index.jsx
@@ -1,13 +1,13 @@
+import React from 'react';
+
 const LeaderboardPlace = ({ building }) => {
+    return (
+        <div className="leaderboard-place">
+            <span className="place-name">{building.name}</span>
+            <span className="place-consumption">{building.consumption} </span>
+            <span className="place-unit">{building.unit}</span>
+        </div>
+    );
+};
 
-  return (
-    <div key={building.name}>
-      <h1>Name: {building.name}</h1>
-      <h2>
-        Consumption: {building.consumption} {building.unit}
-      </h2>
-    </div>
-  );
-}
-
-export default LeaderboardPlace
+export default LeaderboardPlace;
