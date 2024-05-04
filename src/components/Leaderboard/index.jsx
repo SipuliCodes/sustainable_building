b7db643acd83a4a35fc45cd805f18b4362f77f5c
SipuliCodes <vepsalainensisu@gmail.com>
2024-05-04 14:44:28 +0300

    datafetch added

--- src/components/Leaderboard/index.jsx
+++ src/components/Leaderboard/index.jsx
@@ -1,7 +1,27 @@
 import LeaderboardPlace from "./LeaderboardPlace"
 import "./Leaderboard.css"
+import readNDJSONStream from 'ndjson-readablestream'
 
 const Leaderboard = () => {
+  let continuous;
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://hackathon.kvanttori.fi/buildings/d46078e8-8df8-4333-b961-fe8e27ccc130/streams"
+    );
+
+    if (!response.body) return;
+
+    for await (const data of readNDJSONStream(response.body)) {
+      continuous = data
+      console.log(data)
+
+    }
+  }
+
+  console.log(continuous)
+
+  fetchData()
   
   const buildings = [
     {
@@ -32,6 +52,7 @@ const Leaderboard = () => {
         return (<LeaderboardPlace key={building.name} building={building} />)
       }
       )}
+      <h2>{continuous && continuous.consumer.consumption.value}</h2>
     </div>
   )
 }
